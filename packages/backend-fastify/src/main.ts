ce361812117f0473a03b2db8df7b8cca3245054e
hardyeats <hardyeats702@gmail.com>
2022-12-17 15:59:54 +0900

    add ping route

--- packages/backend-fastify/src/main.ts
+++ packages/backend-fastify/src/main.ts
@@ -1,1 +1,11 @@
+import Fastify from "fastify";
 console.log("hello world");
+const server = Fastify();
+server.get("/ping", async () => {
+  return "pong";
+});
+
+const port = Number(process.env.PORT) || 4000;
+server.listen({ port }, () => {
+  console.log(`Server running on port ${port}`);
+});
