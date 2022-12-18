8cb8bcc81122728b05100677cd084f48ef32e13d
hardyeats <hardyeats702@gmail.com>
2022-12-18 13:34:37 +0900

    hardcode signup page

--- packages/frontend-reactjs/src/pages/SignupModal.tsx
+++ packages/frontend-reactjs/src/pages/SignupModal.tsx
@@ -14,6 +14,18 @@ const SignupModal = () => {
           </div>
           <div className={styles.modalHeaderRight}></div>
         </div>
+        <div className={styles.modalBody}>
+          <p>지금 트위터에 가입하세요.</p>
+          <p>구글 로그인</p>
+          <p>애플 로그인</p>
+          <p>-----------또는-----------</p>
+          <p>휴대폰 번호나 이메일 주소로 가입하기</p>
+          <p>
+            가입하시려면 쿠키 사용을 포함해 이용약관과 개인정보 처리방침에
+            동의해야 합니다.
+          </p>
+          <p>이미 계정이 있으신가요? 로그인</p>
+        </div>
       </div>
     </div>
   );
